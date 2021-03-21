75480ffd9d50bd67045cf8c093bbe6dc4b2831b3
Michael Lindahl <me@michaellindahl.com>
2021-03-21 16:22:41 -0700

    Duplicate header for Linux and macOS usage

---
+++ src/preloading/header-so.h
@@ -0,0 +1,43 @@
+#define FFI_SCOPE "H3"
+#define FFI_LIB "libh3.so"
+
+// Where is H3 Library?
+// When installed via Brew on macOS:
+// ./usr/local/lib/libh3.dylib
+// ./usr/local/lib/libh3.1.dylib
+// ./usr/local/Cellar/h3/3.7.1/lib/libh3.dylib
+// ./usr/local/Cellar/h3/3.7.1/lib/libh3.1.dylib
+
+// FFI_LIB uses the system include path, and cwd is NOT included in that by default.
+
+// The header file may contain a #define statement for the FFI_LIB variable to specify the library it exposes. 
+// If it is a system library only the file name is required, e.g.: #define FFI_LIB "libc.so.6". 
+// If it is a custom library, a relative path is required, e.g.: #define FFI_LIB "./mylib.so".
+
+typedef uint64_t H3Index;
+
+typedef struct {
+    /// latitude in radians
+    double lat;  
+    /// longitude in radians
+    double lon;  
+} GeoCoord;
+
+// IndexInspection
+
+int h3GetResolution(H3Index h);
+int h3IsValid(H3Index h);
+
+// Indexing
+
+H3Index geoToH3(const GeoCoord *g, int res);
+void h3ToGeo(H3Index h3, GeoCoord *g);
+
+// HierarchicalGrid
+
+H3Index h3ToParent(H3Index h, int parentRes);
+
+// Misc
+
+double degsToRads(double degrees);
+double radsToDegs(double degrees);
